ffc4fd77eb2778c835dcfdf4a979208e9df904d1
NK <beohemian@gmail.com>
2014-12-06 16:18:32 +0900

    승리 확인 인터페이스 추가.

--- calcurate.h
+++ calcurate.h
@@ -9,6 +9,10 @@
 #define _CALCURATE_
 
 // 외부에 제공될 인터페이스 함수.
+// 플레이어 점수 계산.
 extern void calcurate(void * player);
+// 플레이어가 났는지 확인.
+extern bool is_win(void * player);
 extern void test_show_score(void * player);
+
 #endif /* _CALCURATE_ */
